212cbb30b289754fc11d7526993a6c86fa37c0ea
ibsiany <ibsiany.godinho@precato.com.br>
2022-04-05 08:42:07 -0300

    criando hooks

---
+++ dtmoney/src/hooks/useTransactions.tsx
@@ -0,0 +1,62 @@
+import  {createContext, ReactNode, useContext, useEffect, useState} from 'react';
+import { api } from '../services/api';
+
+interface ITransaction {
+    id: string;
+    title: string;
+    type: string;
+    amount: number;
+    category: string;
+    createdAt: string;
+}
+
+// interface ITransactionInput {
+//     title: string;
+//     type: string;
+//     amount: number;
+//     category: string;
+// }
+
+type TransactionInput = Omit<ITransaction, 'id' | 'createdAt'>
+
+interface ITransactionsProviderProps {
+    children: ReactNode;
+}
+
+interface ITransactionsContextData {
+    transactions: ITransaction[];
+    createTransaction: (transaction: TransactionInput) => Promise<void>;
+}
+
+const TransactionsContext = createContext<ITransactionsContextData>({} as ITransactionsContextData);
+
+export function TransactionsProvider({children}: ITransactionsProviderProps){
+    const [transactions, setTransactions] = useState<ITransaction[]>([]);
+
+    useEffect(() => {
+        api.get('/transactions').then(response => setTransactions(response.data.transactions));
+    }, [])
+
+    async function createTransaction(transactionInput: TransactionInput){
+        const response = await api.post('/transactions', {
+            ...transactionInput,
+            createdAt: new Date()
+        });
+
+        const {transaction} = response.data
+
+        setTransactions([...transactions, transaction])
+    }
+
+    return (
+        <TransactionsContext.Provider value={{transactions, createTransaction}}>
+            {children}
+        </TransactionsContext.Provider>
+    )
+}
+
+export function useTransactions() {
+    const context = useContext(TransactionsContext);
+
+    return context
+}
