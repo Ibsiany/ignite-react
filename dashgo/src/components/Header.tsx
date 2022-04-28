a350ab7293e2ae5827f14502b7b5f886c5b7f96c
ibsiany <ibsiany.godinho@precato.com.br>
2022-04-28 08:59:49 -0300

    graficos

--- dashgo/src/components/Header.tsx
+++ dashgo/src/components/Header.tsx
@@ -1,7 +1,7 @@
-import { Flex, Text,Input, Icon } from '@chakra-ui/react'
-import { RiSearchLine } from 'react-icons/ri'
+import { Flex, Text,Input, Icon, HStack, Box, Avatar } from '@chakra-ui/react'
+import { RiSearchLine, RiNotificationLine, RiUserAddLine } from 'react-icons/ri'
 
-export default function Header(){
+export function Header(){
     return (
         <Flex 
             as="header" 
@@ -46,6 +46,39 @@ export default function Header(){
                 />
                 
                 <Icon as={RiSearchLine} fontSize="20" />
+            </Flex>
+        
+            <Flex
+                align="center"
+                ml="auto"
+            >
+                <HStack 
+                    spacing="8"
+                    mx="8"
+                    pr="8"
+                    py="1"
+                    color="gray.300"
+                    borderRightWidth={1}
+                    borderColor="gray.700"
+                >
+                 <Icon as={RiNotificationLine} fontSize="20" />
+                 <Icon as={RiUserAddLine} fontSize="20" />
+                </HStack>
+
+                <Flex align="center">
+                    <Box mr="4" textAlign="right">
+                        <Text>Ibsiany Dias</Text>
+                        <Text
+                            color="gray.300"
+                            fontSize="small"
+                        >
+                            ibsiany.godinho@precato.com.br
+                        </Text>
+                    </Box>
+
+                    <Avatar size="md" name="Ibsiany Dias" src="https://github.com/ibsiany.png"/>
+
+                </Flex>
             </Flex>
         </Flex>
     )
