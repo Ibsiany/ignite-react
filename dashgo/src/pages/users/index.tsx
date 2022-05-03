3cc2bc34b42890dc552c2149c6725fd8610918be
ibsiany <ibsiany.godinho@precato.com.br>
2022-05-03 08:51:12 -0300

    responsividade de users

--- dashgo/src/pages/users/index.tsx
+++ dashgo/src/pages/users/index.tsx
@@ -1,10 +1,15 @@
-import { Box, Flex, Heading, Button, Icon, Table, Thead, Tr, Th, Checkbox, Tbody, Td, Text } from "@chakra-ui/react";
+import { Box, Flex, Heading, Button, Icon, Table, Thead, Tr, Th, Checkbox, Tbody, Td, Text, useBreakpointValue } from "@chakra-ui/react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
 import { Sidebar } from "../../components/Sidebar";
 import { Pagination } from "../../components/Pagination";
 
 export default function UserList(){
+    const isWideVersion = useBreakpointValue({
+        base: false,
+        lg: true,
+    })
+
   return (
       <Box>
             <Header />
@@ -23,17 +28,17 @@ export default function UserList(){
                     <Table colorScheme="whiteAlpha">
                         <Thead>
                             <Tr>
-                                <Th px="6" color="gray.300" width="8">
+                                <Th px={["4","4", "6"]} color="gray.300" width="8">
                                     <Checkbox colorScheme="pink"/>
                                 </Th>
                                 <Th>Usuário</Th>
-                                <Th>Data de acadastro</Th>
+                                {isWideVersion && <Th>Data de acadastro</Th>}
                                 <Th width="8"></Th>
                             </Tr>
                         </Thead>
                         <Tbody>
                             <Tr>
-                                <Td px="6" >
+                                <Td px={["4","4", "6"]} >
                                     <Checkbox colorScheme="pink"/>
                                 </Td>
                                 <Td>
@@ -42,12 +47,14 @@ export default function UserList(){
                                         <Text fontWeight="bold">ibsiany.godinho@precato.com.br</Text>
                                     </Box>
                                 </Td>
-                                <Td>29 de Abril, 2022</Td>
-                                <Td>
-                                    <Button as="a" size="sm" fontSize="sm" colorScheme="purple" leftIcon={<Icon as={RiPencilLine}/>}>
-                                        Editar
-                                    </Button>
-                                </Td>
+                                {isWideVersion && <Td>29 de Abril, 2022</Td>}
+                                {isWideVersion && 
+                                    <Td>
+                                        <Button as="a" size="sm" fontSize="sm" colorScheme="purple" leftIcon={<Icon as={RiPencilLine}/>}>
+                                            Editar
+                                        </Button>
+                                    </Td>
+                                }
                             </Tr>
                         </Tbody>
                     </Table>
