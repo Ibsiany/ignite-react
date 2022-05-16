74f61b6c93718d027961ba0a557949677c5a6538
ibsiany <ibsiany.godinho@precato.com.br>
2022-05-16 08:39:03 -0300

    começando autenticação

---
+++ next-auth/pages/index.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from 'next'
+import { useState } from 'react';
+import styles from '../styles/Home.module.css'
+
+const Home: NextPage = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('')
+
+  return (
+    <div className={styles.container}>
+      <input type="email" value={email}/>
+    </div>
+  )
+}
+
+export default Home
