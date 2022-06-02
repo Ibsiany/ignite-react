ce6f3760da6fabda8ae72b43dc645bb68b0c28d1
ibsiany <ibsiany.godinho@precato.com.br>
2022-06-02 08:28:53 -0300

    fix: corrigindo configuração de teste

--- ignews/jest.config.js
+++ ignews/jest.config.js
@@ -1,11 +1,18 @@
 module.exports = {
-    testPathIngorePatterns: ["/node_modules", "next"],
-    setupFilesAfterEnv: [
-        "<rootDir>/scr/tests/setupTests,ts"
-    ],
+    testPathIgnorePatterns: ['/mode_modules', '/.next'],
+    roots: ["<rootDir>/src"],
+  
     transform: {
-        "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest"
+      "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest"
     },
+  
+    setupFilesAfterEnv: [
+      "@testing-library/jest-dom/extend-expect"
+    ],
+  
+    testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
+  
+    moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
     testEnvironment: 'jsdom',
     moduleNameMapper : {
         "\\.(scss|css|sass)$": "identity-obj-proxy"
